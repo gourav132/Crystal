d52a53fc0e4e380cd72df2961bdeacb8f536c236
Gourav Chatterjee <gouravchatterjee65@gmail.com>
2024-07-14 04:53:53 +0530

    URL Redirect

--- src/page/Loading/Loading.jsx
+++ src/page/Loading/Loading.jsx
@@ -1,17 +1,18 @@
 import React, { useEffect } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth } from '../../Firebase/config'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export default function Loading() {
     const [ user, loading ] = useAuthState(auth);
     const navigate = useNavigate();
+    const { uid } = useParams();
     useEffect(() => {
-        if(!loading) {
+        if(!loading && !uid) {
             if(user) navigate(`/Gallery/${user.displayName}`);
-            // else navigate('/Auth');
+            else navigate('/Auth');
         }
-    }, [user, navigate, loading])
+    }, [user, navigate, loading, uid])
   return (
     <div className='h-screen w-full flex items-center justify-center absolute top-0 bg-gray-900/10'>
         <p className='text-xl font-semibold text-primary'>Loading...</p>
